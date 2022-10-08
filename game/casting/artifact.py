a09b0e543f986f0a7c91034a25415d3a1a195688
simplelatai <104492814+simplelatai@users.noreply.github.com>
2022-10-08 22:17:40 +1200

    First commit

---
+++ game/casting/artifact.py
@@ -0,0 +1,20 @@
+from game.casting.actor import Actor
+
+# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+    
+    def __init__(self):
+        super().__init__()
+        self._points = 0
+        
+    def get_points(self):
+        if (self.get_text() == "*"):
+            self._points = 1
+            
+        else:
+            self._points = -1
+            
+        return self.points
+    
+
+
